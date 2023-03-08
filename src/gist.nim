5e3ab34cc8461d71896bf48f14d38c1cae3b6f5a
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-03-08 18:05:54 +0700

    get gist
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- src/gist.nim
+++ src/gist.nim
@@ -1,5 +1,48 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import std/[ httpclient, json, tables, os ]
 
-when isMainModule:
-  echo("Hello, World!")
+proc gGist(username: string)
+proc gHelp()
+
+
+# main section
+let input: seq[string] = commandLineParams()
+
+if input.len == 0:
+  gHelp()
+else:
+  echo input
+
+
+proc gGist(username: string) =
+  let  url: string = "https://api.github.com/users/" & username & "/gists"
+  var  responseBody: string
+
+  try:
+    responseBody = newHttpClient().getContent(url)
+  except:
+    echo "Connection Error"
+
+  let
+    responseJson: JsonNode = responseBody.parseJson
+    responseLength: int = responseJson.len
+
+  try:
+    # Printing all key that important
+    for i in 0 .. responseLength - 1 :
+      let
+        jsons: JsonNode = responseJson[i]
+        jsKey: OrderedTable[string, JsonNode] = jsons["files"].getFields
+
+      echo "description :\t", jsons["description"].getStr
+      echo " - url      :\t", jsons["html_url"].getStr
+      echo " - public   :\t", jsons["public"].getBool
+
+      stdout.write " - file     :\t"
+      for i in jsKey.keys: stdout.write i, " "
+
+      echo "\n"
+  except:
+    echo "NULL"
+
+proc gHelp() =
+  echo getAppFilename().extractFilename(), " -g/--get:<username>"
